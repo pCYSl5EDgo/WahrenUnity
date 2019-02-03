d427609427f9ee35b050ec70c9238e2e33a3b916
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2019-02-03 20:06:49 +0900

    [add]Identifierと数値のペアを統一的に扱うリスト

---
+++ Assets/Wahren/Scripts/BasicData/IdentifierNumberPairList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+
+namespace pcysl5edgo.Wahren
+{
+    public unsafe struct IdentifierNumberPairList : IDisposable
+    {
+        public int Capacity;
+        public int Length;
+        public IdentifierNumberPair* Values;
+
+        public bool TryAddMultiThread(in IdentifierNumberPair pair)
+        {
+            int last;
+            do
+            {
+                last = Length;
+                if (last == Capacity)
+                    return false;
+            } while (last != Interlocked.CompareExchange(ref Length, last + 1, last));
+            Values[last] = pair;
+            return true;
+        }
+
+        public void Lengthen()
+        {
+            if (Capacity == 0) return;
+            int size = sizeof(IdentifierNumberPair) * Capacity;
+            var _ = UnsafeUtility.Malloc(size * 2, 4, Allocator.Persistent);
+            UnsafeUtility.MemCpy(_, Values, size);
+            UnsafeUtility.Free(Values, Allocator.Persistent);
+            Values = (IdentifierNumberPair*)_;
+            Capacity *= 2;
+        }
+        public void Dispose()
+        {
+            if (Capacity != 0)
+            {
+                UnsafeUtility.Free(Values, Allocator.Persistent);
+                this = default;
+            }
+        }
+    }
+}
