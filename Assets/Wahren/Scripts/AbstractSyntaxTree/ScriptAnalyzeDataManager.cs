cdf0665e81846c7b0ec77e5b411ea10338d4ec34
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2019-02-04 19:22:11 +0900

    [add]データ集積

---
+++ Assets/Wahren/Scripts/AbstractSyntaxTree/ScriptAnalyzeDataManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace pcysl5edgo.Wahren.AST
+{
+    public unsafe class ScriptAnalyzeDataManager
+    {
+        ScriptAnalyzeDataManager_Internal* _ScriptAnalyzeDataManager;
+    }
+
+    internal unsafe struct ScriptAnalyzeDataManager_Internal
+    {
+        internal TextFile* Files;
+        internal int FileLength;
+        internal RaceParserTempData RaceParserTempData;
+        internal IdentifierNumberPairList IdentifierNumberPairList;
+    }
+}
