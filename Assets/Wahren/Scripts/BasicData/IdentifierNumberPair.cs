b7c5fbe08c3eece434402a6b140c55fbea436c02
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2019-02-04 19:25:48 +0900

    [add]数値に関するエラーをキャプチャしやすくした

--- Assets/Wahren/Scripts/BasicData/IdentifierNumberPair.cs
+++ Assets/Wahren/Scripts/BasicData/IdentifierNumberPair.cs
@@ -4,5 +4,21 @@
     {
         public Span Span;
         public long Number;
+#if UNITY_EDITOR
+        public Span NumberSpan;
+#endif
+
+        public IdentifierNumberPair(Span span, long number
+#if UNITY_EDITOR
+        , Span numberSpan
+#endif        
+        )
+        {
+            Span = span;
+            Number = number;
+#if UNITY_EDITOR
+            NumberSpan = numberSpan;
+#endif
+        }
     }
 }
