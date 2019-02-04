ebf9628da33dddf2a0837250d5080b55d3c151ad
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2019-02-04 19:21:25 +0900

    [modify]VerifyUtility

--- Assets/Wahren/Scripts/Utility/VerifyUtility.cs
+++ Assets/Wahren/Scripts/Utility/VerifyUtility.cs
@@ -9,13 +9,13 @@ namespace pcysl5edgo.Wahren
         public static TryInterpretReturnValue IsCurrentCharEquals(this ref TextFile file, Caret caret, char c)
         {
 
-            if(file.CurrentChar(caret) == c)
+            if (file.CurrentChar(caret) == c)
             {
-                return new TryInterpretReturnValue(new Span(caret, 1), SuccessSentence.LeftBraceConfirmationSuccess, 0, true);
+                return new TryInterpretReturnValue(new Span(caret, 1), SuccessSentence.LeftBraceConfirmationSuccess, InterpreterStatus.Success);
             }
             else
             {
-                return new TryInterpretReturnValue(new Span(caret, 1), ErrorSentence.ExpectedCharNotFoundError, 1, false);
+                return new TryInterpretReturnValue(new Span(caret, 1), ErrorSentence.ExpectedCharNotFoundError, 1, InterpreterStatus.Error);
             }
         }
     }
