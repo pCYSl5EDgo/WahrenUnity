63fe3cc7c19484f857d4c10a72d70b8149edfb3f
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2019-02-05 12:12:17 +0900

    [add]GetHashCodeをSpanに実装

--- Assets/Wahren/Scripts/BasicData/Span.cs
+++ Assets/Wahren/Scripts/BasicData/Span.cs
@@ -37,6 +37,7 @@ namespace pcysl5edgo.Wahren
         public override string ToString() => $"({File}, {Line}, {Column}, {Length})";
         public override bool Equals(object obj) => !(obj is null) && Equals((Span)obj);
         public bool Equals(Span span) => File == span.File && Line == span.Line && Column == span.Column && Length == span.Length;
+        public override int GetHashCode() => (File << 24) ^ (Line << 16) ^ (Column << 8) ^ Length;
         public static bool operator ==(in Span span0, in Span span1) => span0.File == span1.File && span0.Line == span1.Line && span0.Column == span1.Column && span0.Length == span1.Length;
         public static bool operator !=(in Span span0, in Span span1) => span0.File != span1.File || span0.Line != span1.Line || span0.Column != span1.Column || span0.Length != span1.Length;
     }
