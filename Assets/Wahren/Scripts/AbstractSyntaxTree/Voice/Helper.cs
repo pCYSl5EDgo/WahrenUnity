fae1c64e17f3a53a5a11d4beebbacc761200a76d
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2019-02-21 09:18:34 +0900

    [delete]VoiceHelper.cs

--- Assets/Wahren/Scripts/AbstractSyntaxTree/Voice/Helper.cs
+++ Assets/Wahren/Scripts/AbstractSyntaxTree/Voice/Helper.cs
@@ -2,9 +2,9 @@
 {
     internal static class VoiceHelper
     {
-        public static TryInterpretReturnValue CreatePending(this VoiceTree.Kind kind, Span span)
-        => TryInterpretReturnValue.CreatePending(span, Location.Voice, PendingReason.SectionListCapacityShortage, (int)kind);
-        public static ASTValueTypePair CreateASTPair(this VoiceTree.Kind kind)
-        => new ASTValueTypePair((int)kind);
+        // public static TryInterpretReturnValue CreatePending(this VoiceTree.Kind kind, Span span)
+        // => TryInterpretReturnValue.CreatePending(span, Location.Voice, PendingReason.SectionListCapacityShortage, (int)kind);
+        // public static ASTValueTypePair CreateASTPair(this VoiceTree.Kind kind)
+        // => new ASTValueTypePair((int)kind);
     }
 }
