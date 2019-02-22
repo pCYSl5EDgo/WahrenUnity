c3c5327888196a2d50f1d33358e7878cfce02ea0
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2019-02-22 13:05:13 +0900

    [add]LinkedListによってmallocをpage管理する

---
+++ Assets/Wahren/Scripts/BasicData/LinkedList.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Threading;
+using System.Runtime.InteropServices;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+
+namespace pcysl5edgo.Wahren.AST
+{
+    public unsafe interface ILinkedList<TValue, TNode>
+        where TValue : unmanaged
+        where TNode : unmanaged, ILinkedListNode<TValue, TNode>
+    {
+        void Dispose(Allocator allocator);
+        void AddRange(TValue* values, int length, out TNode* page, out int start, Allocator allocator);
+    }
+
+    [StructLayout(LayoutKind.Explicit)]
+    public unsafe struct ListLinkedList : ILinkedList<byte, ListLinkedListNode>
+    {
+        [FieldOffset(0)]
+        public volatile ListLinkedListNode* First;
+        [FieldOffset(8)]
+        public volatile int NodeCapacity;
+
+        public ListLinkedList(int capacity, Allocator allocator)
+        {
+            if (capacity < 1) throw new ArgumentOutOfRangeException(capacity.ToString() + " must be greater than 0");
+            First = (ListLinkedListNode*)UnsafeUtility.Malloc(sizeof(ListLinkedListNode), 4, allocator);
+            UnsafeUtility.MemClear(First, sizeof(ListLinkedListNode));
+            NodeCapacity = capacity;
+        }
+
+        public void Dispose(Allocator allocator)
+        {
+            if (First != null)
+            {
+                First->Dispose(allocator);
+                UnsafeUtility.Free(First, allocator);
+            }
+            this = default;
+        }
+        public void AddRange(byte* values, int length, out ListLinkedListNode* page, out int index, Allocator allocator) => AddRange<byte>(values, length, out page, out index, allocator);
+        public void Add<T>(T value, out ListLinkedListNode* page, out int index, Allocator allocator) where T : unmanaged
+        {
+            var tryNode = First;
+            while (true)
+            {
+                if (tryNode->TryAdd(&value, 1, out index))
+                {
+                    page = tryNode;
+                    return;
+                }
+                if (tryNode->NextNodePtr == IntPtr.Zero)
+                {
+                    (page = ListLinkedListNode.Create<T>(NodeCapacity, allocator))->GetRef<T>(index = 0) = value;
+                    while (IntPtr.Zero != Interlocked.CompareExchange(ref tryNode->NextNodePtr, new IntPtr(page), IntPtr.Zero))
+                    {
+                        tryNode = tryNode->Next;
+                    }
+                    return;
+                }
+                tryNode = tryNode->Next;
+            }
+        }
+        public void AddRange<T>(T* values, int length, out ListLinkedListNode* page, out int start, Allocator allocator) where T : unmanaged
+        {
+            var tryNode = First;
+            if (length > NodeCapacity)
+            {
+                NodeCapacity = length;
+                goto ADDNEWPAGE;
+            }
+            while (true)
+            {
+                if (tryNode->TryAdd(values, length, out start))
+                {
+                    page = tryNode;
+                    return;
+                }
+                if (tryNode->NextNodePtr == IntPtr.Zero)
+                {
+                    goto ADDNEWPAGE;
+                }
+                tryNode = tryNode->Next;
+            }
+        ADDNEWPAGE:
+            page = ListLinkedListNode.Create<T>(NodeCapacity, allocator);
+            start = 0;
+            UnsafeUtility.MemCpy(page->Values, values, sizeof(T) * length);
+            while (IntPtr.Zero != Interlocked.CompareExchange(ref tryNode->NextNodePtr, new IntPtr(page), IntPtr.Zero))
+            {
+                tryNode = tryNode->Next;
+            }
+        }
+    }
+}
---
+++ Assets/Wahren/Scripts/BasicData/LinkedListNode.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Threading;
+using System.Runtime.InteropServices;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+
+namespace pcysl5edgo.Wahren.AST
+{
+    public unsafe interface ILinkedListNode<TValue, TNode>
+        where TValue : unmanaged
+        where TNode : unmanaged
+    {
+        ref TValue GetRef(int index);
+        TValue* GetPointer(int index);
+        TNode* Next { get; set; }
+        bool TryAdd(TValue* values, int length, out int start);
+        void Dispose(Allocator allocator);
+    }
+    [StructLayout(LayoutKind.Explicit)]
+    public unsafe struct ListLinkedListNode : ILinkedListNode<byte, ListLinkedListNode>
+    {
+        [FieldOffset(0)]
+        public volatile IntPtr NextNodePtr;
+        [FieldOffset(8)]
+        public volatile void* Values;
+        [FieldOffset(16)]
+        public volatile int Capacity;
+        [FieldOffset(20)]
+        public volatile int Length;
+
+        public ListLinkedListNode(void* values, int capacity)
+        {
+            NextNodePtr = IntPtr.Zero;
+            Values = values;
+            Capacity = capacity;
+            Length = 0;
+        }
+        public ListLinkedListNode(int capacity, int size, Allocator allocator)
+        {
+            Capacity = capacity;
+            Length = 0;
+            NextNodePtr = IntPtr.Zero;
+            Values = UnsafeUtility.Malloc(size * capacity, 4, allocator);
+            UnsafeUtility.MemClear(Values, size);
+        }
+
+        public static ListLinkedListNode* Create<T>(int capacity, Allocator allocator) where T : unmanaged
+        {
+            var answer = (ListLinkedListNode*)UnsafeUtility.Malloc(sizeof(ListLinkedListNode), 4, allocator);
+            *answer = new ListLinkedListNode(capacity, sizeof(T), allocator);
+            return answer;
+        }
+
+        public ref T GetRef<T>(int index) where T : unmanaged => ref ((T*)Values)[index];
+        public T* GetPointer<T>(int index) where T : unmanaged => (T*)Values + index;
+        public ref byte GetRef(int index) => ref GetRef<byte>(index);
+        public byte* GetPointer(int index) => GetPointer(index);
+        public ListLinkedListNode* Next
+        {
+            get => (ListLinkedListNode*)NextNodePtr.ToPointer();
+            set => NextNodePtr = new IntPtr(value);
+        }
+
+        public void Dispose(Allocator allocator)
+        {
+            if (NextNodePtr != IntPtr.Zero)
+            {
+                var next = Next;
+                next->Dispose(allocator);
+                UnsafeUtility.Free(next, allocator);
+            }
+            if (Capacity != 0)
+            {
+                UnsafeUtility.Free(Values, allocator);
+            }
+            this = default;
+        }
+
+        public bool TryAdd<T>(T* values, int length, out int start) where T : unmanaged
+        {
+            do
+            {
+                start = Length;
+                if (start + length > Capacity)
+                    return false;
+            } while (start != Interlocked.CompareExchange(ref Length, start + length, start));
+            UnsafeUtility.MemCpy(GetPointer<T>(start), values, sizeof(T) * length);
+            return true;
+        }
+        public bool TryAdd(byte* values, int length, out int start) => TryAdd<byte>(values, length, out start);
+    }
+}
